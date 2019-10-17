821e957e9f5e098237658e33d819b307accd4cf8
billran <kentra@pacbell.net>
2019-10-16 18:04:35 -0700

    wip

---
+++ PyShift/workschedule/rotation.py
@@ -0,0 +1,134 @@
+from datetime import datetime, timedelta, date
+
+from PyShift.workschedule.named import Named
+from PyShift.workschedule.localizer import Localizer
+from PyShift.workschedule.day_off import DayOff
+from PyShift.workschedule.rotation_segment import RotationSegment 
+
+##
+# Class Rotation maintains a sequenced list of shift and off-shift time
+# periods.
+# 
+class Rotation(Named):
+    dayOff = None  
+      
+    def __init__(self, name=None, description=None):
+        super().__init__(name, description)
+        self.rotationSegments = []
+        self.periods = None
+    
+    @staticmethod    
+    def getDayOff():
+        if (Rotation.dayOff is None):
+            midnight = datetime.combine(date.today(), datetime.min.time())
+            dayOff = DayOff("DAY_OFF", "24 hour off period", midnight, timedelta(hours=24))
+        return dayOff
+    
+    ##
+    # Get the shifts and off-shifts in the rotation
+    # 
+    # @return List of periods
+    #
+    def getPeriods(self):
+        if (self.periods is None):
+            self.periods = []
+            
+            # sort by sequence number
+            sorted(self.rotationSegments, key=RotationSegment.compareTo)
+
+            for segment in self.rotationSegments:
+                # add the on days
+                if (segment.startingShift is not None):
+                    for _i in range(segment.daysOn):
+                        self.periods.append(segment.startingShift)
+
+                # add the off days
+                for _i in range(segment.daysOff):
+                    self.periods.append(Rotation.getDayOff())
+
+        return self.periods
+    
+    ##
+    # Get the number of days in the rotation
+    # 
+    # @return Day count
+    #
+    def getDayCount(self):
+        return self.periods.count()
+
+    ##
+    # Get the duration of this rotation
+    # 
+    # @return timedelta
+    #
+    def getDuration(self):
+        return timedelta(days=self.periods.count())
+    
+    ##
+    # Get the shift rotation's total working time
+    # 
+    # @return timedelta of working time
+    #
+    def getWorkingTime(self):
+        workingTime = timedelta()
+
+        for period in self.periods:
+            if (period.isWorkingPeriod()):
+                workingTime = workingTime.plus(period.getDuration())
+            
+        
+        return workingTime
+
+    ##
+    # Add a working period to this rotation. A working period starts with a
+    # shift and specifies the number of days on and days off
+    # 
+    # @param startingShift
+    #           {@link Shift} that starts the period
+    # @param daysOn
+    #            Number of days on shift
+    # @param daysOff
+    #            Number of days off shift
+    # @return {@link RotationSegment}
+    #
+    def addSegment(self, startingShift, daysOn, daysOff):
+        if (startingShift is None):
+            msg = Localizer.instance().langStr("no.starting.shift")
+            raise Exception(msg)
+        
+        segment = RotationSegment(startingShift, daysOn, daysOff, self)
+        self.rotationSegments.add(segment)
+        segment.setSequence(self.rotationSegments.count())
+        return segment
+    
+    def compareTo(self, other):
+        return self.name == other.name
+    
+
+    ##
+    # Build a string representation of this rotation
+    #
+    def __str__(self):
+        named = super().__str__()
+        rd = Localizer.instance().langStr("rotation.duration")
+        rda = Localizer.instance().langStr("rotation.days")
+        rw = Localizer.instance().langStr("rotation.working")
+        rper = Localizer.instance().langStr("rotation.periods")
+        on = Localizer.instance().langStr("rotation.on")
+        off = Localizer.instance().langStr("rotation.off")
+
+        periods= ""
+
+        for period in self.periods:
+            if (len(periods) > 0):
+                periods = periods + ", "
+            
+            onOff = on if period.isWorkingPeriod() else off
+            periods = periods + period.name + " (" + str(onOff) + ")"
+        
+
+        text = named + "\n" + rper + ": [" + periods+ "], " + rd + ": " + self.getDuration() + ", " + rda + ": " + timedelta(days=self.getDuration()) + ", " + rw + ": " + self.getWorkingTime()
+
+        return text
+
+        
---
+++ PyShift/workschedule/rotation_segment.py
@@ -0,0 +1,26 @@
+
+##
+# This class represents part of an entire rotation. The segment starts with a
+# shift and includes a count of the number of days on followed by the number of
+# days off.
+# 
+class RotationSegment():
+    def __init__(self, startingShift, daysOn, daysOff, rotation):
+        self.startingShift = startingShift
+        self.daysOn = daysOn
+        self.daysOff = daysOff
+        self.rotation = rotation
+        self.segment = 0
+        
+    ##
+    # Compare two rotation segments
+    # @param other {@link RotationSegment}
+    # @return -1 if starts before other, 0 is same starting times, else 1
+    #
+    def compareTo(self, other):
+        value = 0
+        if (self.sequence < other.sequence):
+            value = -1
+        elif (self.sequence > other.sequence):
+            value = 1
+        return value
