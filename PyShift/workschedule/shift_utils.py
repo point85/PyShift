6a99674f4e4b298d759220affe740cd76c811183
billran <kentra@pacbell.net>
2019-10-22 18:27:05 -0700

    wip

--- PyShift/workschedule/shift_utils.py
+++ PyShift/workschedule/shift_utils.py
@@ -1,13 +1,17 @@
 from builtins import staticmethod
-from datetime import datetime, timedelta, date
+from datetime import datetime
 
 class ShiftUtils():
-
     @staticmethod
-    def toEpochDay(startDate):
+    def toEpochSecond(startDate):
         # seconds from Unix epoch
         delta = startDate - datetime.datetime(1970,1,1)
         totalSeconds = int(delta).total_second
+        return totalSeconds
+
+    @staticmethod
+    def toEpochDay(startDate):
+        totalSeconds = ShiftUtils.toEpochSecond(startDate)
         day = totalSeconds/86400
         remainder = totalSeconds%86400  
         
--- PyShift/workschedule/work_schedule.py
+++ PyShift/workschedule/work_schedule.py
@@ -1,4 +1,9 @@
 from PyShift.workschedule.named import Named
+from PyShift.workschedule.localizer import Localizer
+from PyShift.workschedule.shift import Shift
+from PyShift.workschedule.team import Team
+from PyShift.workschedule.non_working_period import NonWorkingPeriod
+from datetime import timedelta
 
 ##
 # Class WorkSchedule represents a named group of teams who collectively work
@@ -17,6 +22,401 @@ class WorkSchedule(Named):
     #             exception
     def __init__(self, name=None, description=None):
         super().__init__(name, description)
+        self.teams = []
+        self.shifts = []
+        self.nonWorkingPeriods = []
+        
+    ##
+    # Remove this team from the schedule
+    # 
+    # @param team
+    #            {@link Team}
+    def deleteTeam(self, team):
+        if (team in self.teams):
+            self.teams.remove(team)
+
+    ##
+    # Remove a non-working period from the schedule
+    # 
+    # @param period
+    #            {@link NonWorkingPeriod}
+    def deleteNonWorkingPeriod(self, period):
+        if (period in self.nonWorkingPeriods):
+            self.nonWorkingPeriods.remove(period)
+            
+    @staticmethod
+    def getPeriodKey(shift):
+        return shift.startTime
+    ##
+    # Get the list of shift instances for the specified date that start in that
+    # date
+    # 
+    # @param day
+    #            LocalDate
+    # @return List of {@link ShiftInstance}
+    def getShiftInstancesForDay(self, day):
+        workingShifts = []
+
+        # for each team see if there is a working shift
+        for team in self.teams:
+            instance = team.getShiftInstanceForDay(day)
+
+            if (instance is None):
+                continue
+            
+            # check to see if this is a non-working day
+            addShift = True
+
+            startDate = instance.getStartTime().date()
+
+            for nonWorkingPeriod in self.nonWorkingPeriods:
+                if (nonWorkingPeriod.isInPeriod(startDate)):
+                    addShift = False
+                    break
+            
+            if (addShift):
+                workingShifts.add(instance)
+        
+        sorted(workingShifts, key=WorkSchedule.getPeriodKey)
+
+        return workingShifts
+
+    ##
+    # Get the list of shift instances for the specified date and time of day
+    # 
+    # @param dateTime
+    #            Date and time of day
+    # @return List of {@link ShiftInstance}
+    def getShiftInstancesForTime(self, dateTime):
+        workingShifts = []
+
+        # day
+        candidateShifts = self.getShiftInstancesForDay(dateTime.date())
+
+        # check time now
+        for instance in candidateShifts:
+            if (instance.shift.isInShift(dateTime.time())):
+                workingShifts.add(instance)
+        
+        return workingShifts
+
+    ##
+    # Create a team
+    # 
+    # @param name
+    #            Name of team
+    # @param description
+    #            description
+    # @param rotation
+    #            rotation
+    # @param rotationStart 
+    #            Start of rotation
+    # @return {@link Team}
+    def createTeam(self, name, description, rotation, rotationStart):
+        team = Team(name, description, rotation, rotationStart)
+
+        if (team in self.teams):
+            msg = Localizer.instance().langStr("team.already.exists").format(name)
+            raise Exception(msg)
+    
+        self.teams.add(team)
+        team.workSchedule = self
+        return team
+    
+    ##
+    # Create a shift
+    # 
+    # @param name
+    #            Name of shift
+    # @param description
+    #            Description of shift
+    # @param start
+    #            start time of day
+    # @param duration
+    #            duration
+    # @return {@link Shift}
+    def createShift(self, name, description, start, duration):
+        shift = Shift(name, description, start, duration)
+
+        if (shift in self.shifts):
+            msg = Localizer.instance().langStr("shift.already.exists").format(name)
+            raise Exception(msg)
+    
+        self.shifts.add(shift)
+        shift.workSchedule = self
+        return shift
+
+    ##
+    # Delete this shift
+    # 
+    # @param shift
+    #            {@link Shift} to delete
+    def deleteShift(self, shift):
+        if (shift not in self.shifts):
+            return
+    
+        # can't be in use
+        for inUseShift in self.shifts:
+            for team in self.teams:
+                rotation = team.rotation
+
+                for period in rotation.periods:
+                    if (period == inUseShift):
+                        msg = Localizer.instance().langStr("shift.in.use").format(shift.getName())
+                        raise Exception(msg)
+                
+        self.shifts.remove(shift)
+
+    ##
+    # Create a non-working period of time
+    # 
+    # @param name
+    #            Name of period
+    # @param description
+    #            Description of period
+    # @param startDateTime
+    #            Starting date and time of day
+    # @param duration
+    #            Duration of period
+    # @return {@link NonWorkingPeriod}
+    def createNonWorkingPeriod(self, name, description, startDateTime, duration):
+        period = NonWorkingPeriod(name, description, startDateTime, duration)
+
+        if (period in self.nonWorkingPeriods):
+            msg = Localizer.instance().langStr("nonworking.period.already.exists").format(name)
+            raise Exception(msg)
+    
+        period.workSchedule = self
+        self.nonWorkingPeriods.add(period)
+
+        sorted(self.nonWorkingPeriods, key=WorkSchedule.getPeriodKey)
+
+        return period
+
+
+    ##
+    # Get total duration of rotation across all teams.
+    # 
+    # @return Duration of rotation
+    def getRotationDuration(self):
+        timeSum = timedelta()
+
+        for team in self.teams:
+            timeSum = timeSum + team.getRotationDuration()
+    
+        return timeSum
+
+    ##
+    # Get the total working time for all team rotations
+    # 
+    # @return rotation working time
+    def getRotationWorkingTime(self):
+        timeSum = timedelta()
+
+        for team in teams:
+            timeSum = timeSum + team.rotation.getWorkingTime()
+    
+        return timeSum
+
+    ##
+    # Calculate the scheduled working time between the specified dates and
+    # times of day. Non-working periods are removed.
+    # 
+    # @param from
+    #            Starting date and time
+    # @param to
+    #            Ending date and time
+    # @return Working time duration
+    # @throws Exception
+    #             exception
+
+    def calculateWorkingTime(self, fromTime, toTime):
+        timeSum = timedelta()
+
+        # now add up scheduled time by team
+        for team in self.teams:
+            timeSum = timeSum + team.calculateWorkingTime(fromTime, toTime)
+    
+        # remove the non-working time
+        nonWorking = self.calculateNonWorkingTime(fromTime, toTime)
+        timeSum = timeSum - nonWorking
+
+        # clip if negative
+        if (timeSum < 0):
+            timeSum = timedelta()
+
+        return timeSum
+
+    ##
+    # Calculate the non-working time between the specified dates and times of
+    # day.
+    # 
+    # @param from
+    #            Starting date and time
+    # @param to
+    #            Ending date and time
+    # @return Non-working time duration
+    def calculateNonWorkingTime(self, fromTime, toTime):
+        timeSum = timedelta()
+
+        fromSeconds = ShiftUtils.toEpochSecond(fromTime)
+        toSeconds = ShiftUtils.toEpochSecond(toTime)
+
+        for period in self.nonWorkingPeriods:
+            start = period.startTime
+            startSeconds = ShiftUtils.toEpochSecond(start)
+
+            end = period.getEndDateTime()
+            endSeconds = ShiftUtils.toEpochSecond(end)
+
+            if (fromSeconds >= endSeconds):
+                # look at next period
+                continue
+        
+            if (toSeconds <= startSeconds):
+                # done with periods
+                break
+    
+            if (fromSeconds <= endSeconds):
+                # found a period, check edge conditions
+                if (fromSeconds > startSeconds):
+                    startSeconds = fromSeconds
+            
+                if (toSeconds < endSeconds):
+                    endSeconds = toSeconds
+
+                timeSum = timeSum + (endSeconds - startSeconds)
+        
+            if (toSeconds <= endSeconds):
+                break
+            
+        return timeSum
+
+    ##
+    # Print shift instances
+    # 
+    # @param start
+    #            Starting date
+    # @param end
+    #            Ending date
+    # @throws Exception
+    #             exception
+
+    def printShiftInstances(self, start, end):
+        if (start > end):
+            msg = Localizer.instance().langStr("end.earlier.than.start").format(start, end)
+            raise Exception(msg)
+    
+        days = ShiftUtils.toEpochDay(end) - ShiftUtils.toEpochDay(start) + 1
+        day = start
+
+        print(Localizer.instance().langStr("shifts.working"))
+        for i in range(days):
+            print("[" + str(i + 1) + "] " + Localizer.instance().langStr("shifts.day") + ": " + str(day))
+
+            instances = self.getShiftInstancesForDay(day)
+
+            if (len(instances) == 0):
+                print("   " + Localizer.instance().langStr("shifts.non.working"))
+        else:
+            count = 1
+            for instance in instances:
+                print("   (" + str(count) + ")" + str(instance))
+                count = count + 1
+        
+            day = day + timedelta(days=1)
+
+    ##
+    # Build a string value for the work schedule
+    # 
+    # @return String
+
+    @Override
+    def toString():
+        DecimalFormat df = DecimalFormat()
+        df.setMaximumFractionDigits(2)
+        sch = getMessage("schedule")
+        rd = getMessage("rotation.duration")
+        sw = getMessage("schedule.working")
+        sf = getMessage("schedule.shifts")
+        st = getMessage("schedule.teams")
+        sc = getMessage("schedule.coverage")
+        sn = getMessage("schedule.non")
+        stn = getMessage("schedule.total")
+
+        text = sch + ": " + super.toString()
+        try:
+            text += "\n" + rd + ": " + getRotationDuration() + ", " + sw + ": " + getRotationWorkingTime()
+
+            # shifts
+            text += "\n" + sf + ": "
+            int count = 1
+            for (shift : getShifts()):
+                text += "\n   (" + count + ") " + shift
+                count++
+        
+
+            # teams
+            text += "\n" + st + ": "
+            count = 1
+            float teamPercent = 0.0f
+            for (team : self.getTeams()):
+                text += "\n   (" + count + ") " + team
+                teamPercent += team.getPercentageWorked()
+                count++
+        
+            text += "\n" + sc + ": " + df.format(teamPercent) + "%"
+
+            # non-working periods
+            List<NonWorkingPeriod> periods = getNonWorkingPeriods()
+
+            if (periods.size() > 0):
+                text += "\n" + sn + ":"
+
+                totalMinutes = Duration.ZERO
+
+                count = 1
+                for (period : periods):
+                    totalMinutes = totalMinutes.plusMinutes(period.getDuration().toMinutes())
+                    text += "\n   (" + count + ") " + period
+                    count++
+            
+                text += "\n" + stn + ": " + totalMinutes
+        
+     catch (Exception e):
+            # ignore
+    
+
+        return text
+
+
+    ##
+    # Get the optimistic locking version
+    # 
+    # @return version
+
+    def Integer getVersion():
+        return version
+
+
+    ##
+    # Set the optimistic locking version
+    # 
+    # @param version
+    #            Version
+
+    def setVersion(Integer version):
+        self.version = version
+
+
+    ##
+    # Compare one work schedule to another
+
+    @Override
+    def int compareTo(WorkSchedule other):
+        return getName().compareTo(other.getName())
+
+        
     
 
     
